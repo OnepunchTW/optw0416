5e9ce257e6d5c55a8cba92bb19ec3616643b0886
rav4kumar <meetkumardesai@gmail.com>
2025-04-15 20:02:42 -0700

    deep experimental

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_profiles.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_profiles.py
@@ -6,17 +6,17 @@ See the LICENSE.md file in the root directory for more details.
 """
 
 # Acceleration profile for maximum allowed acceleration
-MAX_ACCEL_ECO     = [2.00, 1.90, 1.85, 1.52, 0.90, .59,  .48, .39, .32, .12]
-MAX_ACCEL_NORMAL  = [2.00, 1.95, 1.85, 1.75, 1.10, .75,  .61, .50, .38, .2]
-MAX_ACCEL_SPORT   = [2.00, 2.00, 1.98, 1.90, 1.30, 1.00, .72, .60, .48, .3]
+MAX_ACCEL_ECO     = [2.0, 2.0, 2.0, 1.38, 0.91, .53,  .42,  .31,  .085]
+MAX_ACCEL_NORMAL  = [2.0, 2.0, 2.0, 1.68, 1.07, .72,  .53,  .42,  .13]
+MAX_ACCEL_SPORT   = [2.0, 2.0, 2.0, 2.00, 1.34, .96,  .78,  .60,  .4]
 
 # Acceleration profile for minimum (braking) acceleration
 #MIN_ACCEL_ECO     = [-1.0, -1.0, -1.0, -1.0, -1.0]
-MIN_ACCEL_ECO     = [-0.062, -0.062, -0.08, -1.2, -1.2]
-MIN_ACCEL_NORMAL  = [-0.064, -0.064, -0.09, -1.2, -1.2]
-MIN_ACCEL_SPORT   = [-0.068, -0.068, -0.10, -1.3, -1.3]
-MIN_ACCEL_STOCK   = [-1.2, -1.2, -1.2, -1.2, -1.2]
+MIN_ACCEL_ECO     = [-1.0, -1.0,  -0.74]
+MIN_ACCEL_NORMAL  = [-1.0, -1.0,  -0.76]
+MIN_ACCEL_SPORT   = [-1.0, -1.0,  -0.78]
+MIN_ACCEL_STOCK   = [-1.2, -1.2, -1.2]
 
 # Speed breakpoints for interpolation
-MAX_ACCEL_BREAKPOINTS = [0., 1., 6., 8., 11., 16, 20., 25., 30., 55.]
-MIN_ACCEL_BREAKPOINTS = [0., 0.6, 5., 18, 40]
+MAX_ACCEL_BREAKPOINTS = [0.,  1.,  6.,  8.,   11.,  20.,  25.,  30.,  55.]
+MIN_ACCEL_BREAKPOINTS = [0.,   11.1,  20.]
